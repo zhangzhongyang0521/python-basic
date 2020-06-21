06b1509918f4bd533ef5865865cdf84bc7c8083e
zhangzhongyang <zhangzhongyang0521@126.com>
2020-06-21 21:53:17 +0800

    string in python

---
+++ python-quickstart/string.py
@@ -0,0 +1,28 @@
+title = 'title of the basic python'
+value_cn = "相关函数：type(variableName):查看变量数据类型，id(variableName):查看变量内存地址"
+value_en = '''docker stop <container_id>
+docker save [images] [images] > file.tar'''
+
+print(title)
+print(value_cn)
+print(value_en)
+
+result = "the result is array['username']"
+print(result)
+
+pictureText = '''
+                  ,;,,;
+                 ,;;'(    马
+       __      ,;;' ' \   ┇
+    /'  '\'~~'~' \ /'\.)  到 
+ ,;(      )    /  |.      ┇
+,;' \    /-.,,(   ) \     成
+     ) /       ) / )|     ┇ 
+     ||        ||  \)     功
+     (_\       (_\
+'''
+print(pictureText)
+
+content = "this conetnt:\tpython content"
+print(content)
+print(r"this conetnt:\tpython content")
